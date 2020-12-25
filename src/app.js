7b625efbc42e3963da5f0543492cfa07a27ec891
Oliwer <oe222ez@student.lnu.se>
2020-12-25 22:45:23 +0100

    End-user have to pass an url, installed isUrl to verify if argument is an url

--- src/app.js
+++ src/app.js
@@ -5,17 +5,24 @@
  * @version 1.0.0
  */
 
+import isUrl from 'is-url'
+
 import * as application from './application.js'
 
 function startScraper () {
     // hantera argument
+    if (isUrl(process.argv[2])){
+      // skapa scraper
+      const webScraper = new application.Application(process.argv[2]) // npm start https://cscloud6-127.lnu.se/scraper-site-1  // npm start https://cscloud6-127.lnu.se/scraper-site-2
+      //startar scraper
+      webScraper.firstScrape()
+    } else {
+        throw new Error('The passed argument is not an url!')
+    }
 
 
-    // skapa scraper
-    const webScraper = new application.Application('https://cscloud6-127.lnu.se/scraper-site-2') // https://cscloud6-127.lnu.se/scraper-site-2 // obs glöm ej fånga fel om url inte fungerar!
 
-    //startar scraper
-    webScraper.firstScrape() // ta bort hårdkodning!
+
 }
 
 startScraper()
